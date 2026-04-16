0f8f766d3c049874e77f13d17ed63e5b6490f976
Silvio <silviovasconcelos91@gmail.com>
2026-04-16 10:38:26 +0200

    Je suis con

--- app.config.ts
+++ app.config.ts
@@ -29,7 +29,9 @@ const config: ExpoConfig = {
   },
   extra: {
     apiUrl: process.env.API_URL ?? 'https://matchvolley-production.up.railway.app',
-    projectId: '9bf00671-0e20-45cb-bd36-67c7c70ba5d3'
+    eas :{
+      projectId: '9bf00671-0e20-45cb-bd36-67c7c70ba5d3'
+    }
   },
 };
 
